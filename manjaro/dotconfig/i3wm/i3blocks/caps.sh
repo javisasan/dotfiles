c3000c6da8039bf88543959b5cdfe70aa765be74
Javier Sagra <javisasan@gmail.com>
2022-04-17 16:22:02 +0200

    Change notifications

--- manjaro/dotconfig/i3wm/i3blocks/caps.sh
+++ manjaro/dotconfig/i3wm/i3blocks/caps.sh
@@ -3,6 +3,6 @@
 caps=$(xset q | grep Caps | awk '{print $4}')
 
 if [ "$caps" = "on" ] ; then
-    echo "<span background=\"#f99312\"><b>CAPS</b></span>"
+    echo "<span background=\"#f99312\" color=\"#333333\"><b> CAPS </b></span>"
 fi;
 
--- manjaro/dotconfig/i3wm/i3blocks/network.sh
+++ manjaro/dotconfig/i3wm/i3blocks/network.sh
@@ -8,7 +8,7 @@ vpn=$(ip link | grep tun | awk '{print $2}')
 
 if [ ${vpn} ]
 then
-    vpn_warning="<span background=\"#91E78B\" color=\"#ffffff\"><b>VPN</b></span> "
+    vpn_warning="<span background=\"#91E78B\" color=\"#333333\"><b> VPN </b></span> "
 fi;
 
 host=$(ip addr | grep 192 | awk '{print $2}' | sed "s/\/.*//g")
