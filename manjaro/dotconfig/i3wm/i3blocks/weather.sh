697b2ce5654d194d14dadcf3b6df9e6cc059d4ea
Javier Sagra <javisasan@gmail.com>
2023-04-10 15:31:52 +0200

    Changes to dotfiles

--- manjaro/dotconfig/i3wm/i3blocks/weather.sh
+++ manjaro/dotconfig/i3wm/i3blocks/weather.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-weather=$(curl -Ss 'https://wttr.in?0&T&Q' | cut -c 16- | head -2 | xargs echo)
+#weather=$(curl -Ss 'https://wttr.in?0&T&Q' | cut -c 16- | head -2 | xargs echo)
+weather=$(curl 'wttr.in?format=3' | cut -c 18- | xargs echo)
 
 case $BLOCK_BUTTON in
     1) eval "python3 /home/javi/.config/i3blocks/change_wallpaper.py";;
